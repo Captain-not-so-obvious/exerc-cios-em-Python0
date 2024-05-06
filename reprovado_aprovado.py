6690ee10284bc071abeaee79ebf828c4a3c1b045
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2024-05-06 11:31:46 -0300

    Create reprovado_aprovado.py

---
+++ reprovado_aprovado.py
@@ -0,0 +1,7 @@
+def passei_ou_não(nota):
+  if nota > 7:
+    print(f"Sua nota foi {nota}. Parabéns! Você foi aprovado!")
+  else:
+    print(f"Sua nota foi {nota}. Não foi dessa vez! Tente novamente!")
+
+passei_ou_não(3)
